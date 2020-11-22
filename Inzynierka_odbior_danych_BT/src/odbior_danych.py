cfe1a2711ccdfb5942886c87ad70c4008f8efd59
GregC199 <cieslargrzegorz199@gmail.com>
2020-11-22 19:24:23 +0100

    Dokonczenie opisow funkcji

--- Inzynierka_odbior_danych_BT/src/odbior_danych.py
+++ Inzynierka_odbior_danych_BT/src/odbior_danych.py
@@ -39,18 +39,59 @@ warunek = 0
 
 
 '''
+
+'''
+Sprawdzenie na poczatku czy nawiazanie polaczenia odbylo sie poprawnie
+Jesli nie to wyswietlany jest napis informujacy o bledzie i konczacy tym samym program
+'''
 try:
     conn = serial.Serial(serial_nazwa,baud_rate,timeout=0)
     warunek_polaczenia = 1
 except:
     print(f"Nie udalo sie nawiazac polaczenia z portem {serial_nazwa}")
-
+    exit()
+'''
+Jesli udalo sie otworzyc polaczenie z portem COM to zaczynamy glowna petle programu
+'''
 if warunek_polaczenia == 1:
     print(f'Polaczono z {serial_nazwa}')
+    '''
+    Sprawdzamy wpierw czy istnieje plik o danej nazwie.
+    Nazwa pliku jest generowana w nastepujacy sposob:
+    W zaleznosci od wartosci zmiennej numer_pliku
+    sprawdzamy czy istnieje ju¿ plik o nazwie
+    "danenumer_pliku.txt"
+    Jesli istnieje to zwiekszamy wartosc zmiennej i szukamy
+    pierwszej wolnej nazwy.
+    Jesli nazwa jest wolna to tworzymy plik i otwieramy go
+    do zapisu oraz wyswietlamy napis na wyjsciu standardowym
+    informujacy o powodzeniu operacji i nazwie pliku.
+    '''
     while os.path.exists(f'dane{numer_pliku}.txt'):
         numer_pliku += 1
     file = open(f'dane{numer_pliku}.txt','w')
     print(f'Utworzono plik dane{numer_pliku}.txt')
+    
+    '''
+    Nieskonczona petla programu wewnatrz, ktorej odbywa sie
+    odbior informacji.
+    Dane sa odczytywane z portu COM - serial_nazwa. 
+    Wazna czescia algorytmu odbioru danych jest
+    sprawdzanie polozenia . w kazdej z liczb, gdyz
+    sa wysylane z dokladnoscia do 10^(-6). W ten sposob
+    uodparniamy nasze dane na bledy w komunikacji.
+    Calosc wiadomosci jest scalana przy pomocy zmiennych
+    string oraz mem.
+    Jest to mozliwe dzieki stalej liczbie znakow po "."
+    wewnatrz kazdej z liczb.
+    Na koniec zapisujemy liczby do pliku po 9 komorek 
+    na linie pliku, czyli w nastepujacym formacie:
+    gyr_x gyr_y gyr_z acc_x acc_y acc_z mag_x mag_y mag_z
+    gdzie:
+    gyr - oznacza dane z zyroskopu,
+    acc - oznacza dane z akcelerometru
+    mag - oznacza dane z magnetometru
+    '''
     while True:
         rcv_data = conn.readline()
         if rcv_data:
