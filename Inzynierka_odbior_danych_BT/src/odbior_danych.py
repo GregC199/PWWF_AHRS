36b96eee4ff052d516a62ecc1e403d9365567a33
GregC199 <cieslargrzegorz199@gmail.com>
2020-11-22 04:41:16 +0100

    Modul odbierajacy dane wraz z przykladowym plikiem tekstowym z danymi.

--- Inzynierka_odbior_danych_BT/src/odbior_danych.py
+++ Inzynierka_odbior_danych_BT/src/odbior_danych.py
@@ -11,7 +11,7 @@ from _ast import Try
 '''
 Zmienne zwiazane portem COM
 '''
-serial_nazwa = 'COM6'
+serial_nazwa = "COM6"
 baud_rate = 115200
 timeout = 100
 warunek_polaczenia = 0
@@ -19,7 +19,20 @@ warunek_polaczenia = 0
 Zmienne zwiazane z odczytem danych z portu COM
 '''
 numer_pliku = 0
+
 '''
+Zmienne zwiazane z odczytem danych z portu COM
+'''
+
+iterator = 0
+wiersz = 0
+mem = ""
+kodowanie = "utf-8"
+string = ""
+dodatkowa_pozycja = 0
+warunek = 0
+'''
+
 
 
                               Poczatek programu glownego
@@ -38,3 +51,41 @@ if warunek_polaczenia == 1:
         numer_pliku += 1
     file = open(f'dane{numer_pliku}.txt','w')
     print(f'Utworzono plik dane{numer_pliku}.txt')
+    while True:
+        rcv_data = conn.readline()
+        if rcv_data:
+            data_list = rcv_data.split()
+            #print(data_list)
+            for i in range(len(data_list)):
+                string=data_list[i].decode(kodowanie)
+                if string.find('.')>-1:
+                    dodatkowa_pozycja = dodatkowa_pozycja + string.find('.') - 1
+                    warunek = 1
+                elif warunek == 0:
+                    dodatkowa_pozycja = dodatkowa_pozycja + len(string)
+                if len(string) < (8 + dodatkowa_pozycja):
+                    print(f"string: {string}")
+                    mem = mem + string
+                    print(f"scalam: {mem}")
+                else:
+                    mem = string
+                                
+                
+                if len(mem) > (7 + dodatkowa_pozycja):
+                    print(f"powtorzenie nr {i}")
+                    
+                    string=mem
+                    
+                    iterator = iterator + 1
+                    file.write(string)
+                    file.write(" ")
+                    mem=""
+                    dodatkowa_pozycja = 0
+                    warunek = 0
+                    
+                    if iterator > 8:
+                        file.write("\n")
+                        wiersz = wiersz + 1
+                        iterator = 0
+
+                
